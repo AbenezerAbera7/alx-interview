3b085283167b2eada4fac05dd5d628ff7952586e
Abenezer Abera <abenynin2314@gmail.com>
2023-04-28 03:50:00 -0700

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,55 @@
 #!/usr/bin/python3
-"""Prime number game"""
+"""
+Contains method to determine the winner of a game
+of prime numbers.
+"""
+
+
+def prime_numbers_between(n):
+    """
+    calculate prime numbers between 1 and n
+
+    Args:
+        n (int): the number to calculate prime numbers up to
+
+    Returns:
+        int: the number of prime numbers between 1 and n
+    """
+    prime_numbers = 0
+
+    for i in range(2, n + 1):
+        is_prime = True
+        for j in range(2, i // 2 + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            prime_numbers += 1
+    return prime_numbers
 
 
 def isWinner(x, nums):
-    """Function that performs prime game"""
-    if not nums or x < 1:
+    """
+    Determines the winner of a game of prime numbers.
+
+    Args:
+        x (int): the number of rounds to play
+        nums (list): the list of numbers n to play
+
+    Returns:
+        string: the winner of the game (Ben or Maria)
+    """
+    if not x or not nums:
         return None
-    n = max(nums)
-    tcase = [True for _ in range(max(n + 1, 2))]
-    for i in range(2, int(pow(n, 0.5)) + 1):
-        if not tcase[i]:
-            continue
-        for j in range(i * i, n + 1, i):
-            tcase[j] = False
-    tcase[0] = tcase[1] = False
-    c = 0
-    for i in range(len(tcase)):
-        if tcase[i]:
-            c += 1
-        tcase[i] = c
-    player1 = 0
-    for n in nums:
-        player1 += tcase[n] % 2 == 1
-    if player1 * 2 == len(nums):
+    ben = 0
+    maria = 0
+    for i in range(x):
+        prime_nums = prime_numbers_between(nums[i])
+        if prime_nums % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben == maria:
         return None
-    if player1 * 2 > len(nums):
-        return "Maria"
-    return "Ben
+    winner = "Ben" if ben > maria else "Maria"
+    return winner
